f4913255579ceabfc372de86f98bb18c05c49166
bogusz2233 <bogusz2233@gmail.com>
2021-09-18 12:51:29 +0200

    refactor list_uint8.c

--- example/list_uint8.c
+++ example/list_uint8.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "list.h"
 
+static void print_list_info(list_t *list);
+
 int main(int arg_count, char *arg_values[])
 {
     list_t *list;
@@ -11,8 +13,14 @@ int main(int arg_count, char *arg_values[])
     printf("List example - uint8_t\n");
     list = list_create_uint8();
     
-    printf("List of uint8_t elements created %ld\n", (long int)list);
-    printf("List ptr: %d\n", list->countElements);
-    printf("Element in list: %d\n", list->countElements);
-    printf("HEAD ptr: %ld\n", (long int)list->head);
+    printf("List created\n");
+    print_list_info(list);
+}
+
+static void print_list_info(list_t *list)
+{
+    printf("List ptr:           %ld\n", (long int)list);
+    printf("List elemets:       %d\n", list->countElements);
+    printf("Element in list:    %d\n", list->countElements);
+    printf("HEAD ptr:           %ld\n", (long int)list->head);
 }
