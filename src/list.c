d12b7bd3f823e57deeae8db0167a50ba2d92c218
bogusz2233 <bogusz2233@gmail.com>
2021-09-18 12:45:46 +0200

    added function to create uint8 list

--- src/list.c
+++ src/list.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "list.h"
 
-list_t *list_create_uint8()
+list_t *list_create_uint8(void)
 {
     list_t *list        = malloc(sizeof(list_t));
     list->dataType      = eList_data_type_uint8;
