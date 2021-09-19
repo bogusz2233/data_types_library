60dc064a12c365a89aa6a292411a9db59f33bc8b
bogusz2233 <bogusz2233@gmail.com>
2021-09-19 20:11:18 +0200

    Changed function name to make iterface looks better

--- src/list.h
+++ src/list.h
@@ -22,13 +22,14 @@ typedef enum
 
 list_t              *list_create(void);
 list_size_t         list_count_elements(list_t *list);
-list_data_type_t    list_get_element_type(list_t *list, list_size_t index);
-list_status_t       list_remove_elements(list_t *list, list_size_t index);
 void                list_clear(list_t *list);
 void                list_destroy(list_t *list);
 
-void                list_uint8_add_new_element(list_t *list, uint8_t value);
-uint8_t             list_uint8_get_element(list_t *list, list_size_t index);
-list_status_t       list_uint8_set_element(list_t *list, list_size_t index, uint8_t value);
+list_data_type_t    list_element_get_type(list_t *list, list_size_t index);
+list_status_t       list_element_remove(list_t *list, list_size_t index);
+
+void                list_element_uint8_add_new(list_t *list, uint8_t value);
+uint8_t             list_element_uint8_get(list_t *list, list_size_t index);
+list_status_t       list_element_uint8_set(list_t *list, list_size_t index, uint8_t value);
 
 #endif /* __LIST_H__ */
